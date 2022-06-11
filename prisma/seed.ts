9eabca989fab282f6f630b5c7314759904fbd563
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-06-11 14:29:36 +0200

    Seed the database

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client';
+
+import { links } from '../data/links';
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  await prisma.user.create({
+    data: {
+      email: 'testemail@gmail.com',
+      role: 'ADMIN',
+    },
+  });
+
+  await prisma.link.createMany({
+    data: links,
+  });
+};
+
+main()
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
